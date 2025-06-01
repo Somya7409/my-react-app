372746b539d05882fb44af8ef2d70d17165c46bb
Somya7409 <mt1240090@iitd.ac.in>
2025-06-01 22:32:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
       
-      <body>
+      <div>
         <h1>
           HELLO WORLD 
         </h1>
         I just just completed the weekly task.
-      </body>
+      </div>
   
   );
 }
