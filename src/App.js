218e2a0fa1204d735ed8024b86f8d01f577b6892
Somya Aditya <mt1240090@iitd.ac.in>
2025-06-01 20:16:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      
+      <body>
+        <h1>
+          HELLO WORLD 
+        </h1>
+        I just just completed the weekly task.
+      </body>
+  
+  );
+}
+
+export default App;
